cf45767d4d3e13ed855adbc31603f1c7e2e5bb1f
Lokeswari Satyanarayana <lokeswari.satyanarayana@mutualmobile.com>
2022-02-19 18:09:32 +0530

    Initial Commit

---
+++ DrippingTabBarAnimation/DrippingTabBarAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrippingTabBarAnimationApp.swift
+//  DrippingTabBarAnimation
+//
+//  Created by Lokeswari Satyanarayana on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DrippingTabBarAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
